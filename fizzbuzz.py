8cf472b8a653e59c62249a767f73fb5cd9035f4e
Daizy <kxania@gmail.com>
2018-09-12 16:45:40 +0300

    Add more readable code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,21 +1,23 @@
-list1 = [int(n) for n in raw_input("Enter: ").split(",")]
-print(list1)
-print ("List1 is ") + str(len(list1)) + (" characters long")
+def get_input():
+    
+    list1 = [int(n) for n in input("Enter: ").split(",")]
+    print(list1)
+    print ("List1 is " + str(len(list1)) + " characters long")
 
-list2 = [int(n) for n in raw_input("Enter: ").split(",")]
-print(list2)
-print ("List2 is ") + str(len(list2)) + (" characters long")
+    list2 = [int(n) for n in input("Enter: ").split(",")]
+    print(list2)
+    print ("List2 is " + str(len(list2)) + " characters long")
+    print(Fizzbuzz(list1, list2))
 
-def fizzbuzz(a, b):
-    totallength = int(len(list1)) + int(len(list2))
- 
-    if totallength%3 == 0:
-        print("Fizz")
-    elif totallength%5 == 0:
-        print("Buzz")
-    elif totallength%5 == 0 and totallength%3 == 0:
-        print(Fizzbuzz)
+def Fizzbuzz(a, b):
+    totallength = len(a) + len(b)
+    if totallength % 3 == 0:
+        return 'Fizz'
+    elif totallength % 5 == 0:
+        return 'Buzz'
+    elif totallength % 5 == 0 and totallength % 3 == 0:
+        return 'FizzBuzz'
     else:
-        print(totallength)
+        return totallength
 
-fizzbuzz(list1,list1)
+get_input()
