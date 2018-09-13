a1eee6c9fbc95f8070191bb8dc039cfa6602a927
Daizy <kxania@gmail.com>
2018-09-13 11:12:11 +0300

    Add more readable code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
 def get_input():
     
-    list1 = [int(n) for n in input("Enter: ").split(",")]
+    list1 = [n for n in input("Enter the first list (separate elements with commas): ").split(",")]
     print(list1)
     print ("List1 is " + str(len(list1)) + " characters long")
 
-    list2 = [int(n) for n in input("Enter: ").split(",")]
+    list2 = [n for n in input("Enter the second list (separate elements with commas): ").split(",")]
     print(list2)
     print ("List2 is " + str(len(list2)) + " characters long")
     print(Fizzbuzz(list1, list2))
